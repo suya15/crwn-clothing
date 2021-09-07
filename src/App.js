aab74b9a2de74454e09a52f72946f47495761268
suyash jain <suyash151604@gmail.com>
2021-09-07 21:45:41 +0530

    update out menu-item withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import HomePage from './pages/homepage.component';
 
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      namma
+    </h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div>
-
-      <HomePage />
-
-       
-
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/Hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
