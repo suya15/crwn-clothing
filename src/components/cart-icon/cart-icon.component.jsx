2582f80adac52fb073d1fe9ad572c8ef2eb1ea69
suyash jain <suyash151604@gmail.com>
2021-09-22 12:28:59 +0530

    updated app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,19 +3,44 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
         <span className="item-count">
-            0
+            {itemCount}
         </span>
     </div>
 );
+
+
+// without using selector for optimization
+// const mapStateToProps = ({ cart: {cartItems }}) => (
+//     {
+//         // we have just written a selector here because we have written a code in which we are getting the
+//         // whole state out but pulling a certain value to get our things done.
+
+//         itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => 
+//         accumalatedQuantity + cartItem.quantity, 0)
+//     }
+// )
+
+
+// using selector 
+
+const mapStateToProps = (state) => (
+    {
+        // we have just written a memoize selector here because we have written a code in which we are getting the
+        // whole state out but pulling a certain value to get our things done.
+
+        itemCount: selectCartItemsCount(state)
+    }
+)
 
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden : () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+// input selector
+const selectCart = state => state.cart;
+
+
+// memoize output selectors
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((accumalatedQuantity, cartItem) => 
+    accumalatedQuantity + cartItem.quantity, 0)
+);
